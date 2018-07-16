fbfb916e729be3ff82c01e3e8d1a1fa3bba0a81a
Marco-Malacara <marco.malacara7@gmail.com>
2018-07-16 08:19:37 -0500

    happy numbers upload

---
+++ Happy Numbers/HappyNumbers-script.js
@@ -0,0 +1,101 @@
+'user strict'
+// debugger
+function happyNumbers(){
+    let happyNumbers = getUserInput('Enter a number up to 100.');
+    let num1 = 1
+    let num2 = 7
+    let num3 = 10
+    let num4 = 13
+    let num5 = 19
+    let num6 = 23
+    let num7 = 28
+    let num8 = 31
+    let num9 = 32
+    let num10 = 44
+    let num11 = 49
+    let num12 = 68
+    let num13 = 70
+    let num14 = 79
+    let num15 = 82
+    let num16 = 86
+    let num17 = 91
+    let num18 = 94
+    let num19 = 97
+    let num20 = 100
+
+        if (happyNumbers == num1){
+            console.log ("You found a happy number! Good for you!");
+            }
+        else if(happyNumbers == num2){
+            console.log ("You found a happy number! Good for you!");
+        } 
+        else if(happyNumbers == num3){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num4){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num5){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num6){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num6){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num6){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num7){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num8){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num9){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num10){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num11){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num12){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num13){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num14){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num15){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num16){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num17){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num18){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num19){
+            console.log('You found a happy number! Good for you!');
+        }
+        else if(happyNumbers == num20){
+            console.log('You found a happy number! Good for you!');
+        }
+        else {
+            console.log('You didn\'t find a happy number.. :(. Better luck next time! Or you went over 100...Shame on you!')
+        }
+        }
+                 
+
+function getUserInput(input){
+        return prompt(input)
+}
+happyNumbers()
